02b469cf6997b6a5c24042a47749c71d0e256365
olinskii <nilo_rodriguez@hotmail.com>
2016-10-17 00:29:19 +0100

    Setup Angular 2

---
+++ angular2-practice-01/angular2-practice-01/app/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+//import { AppModule } from './app.module';
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
